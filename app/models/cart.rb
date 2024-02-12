9d9c3a7fea844ca450e74924502527588f742bf9
subudei <milan_cirkovic@yahoo.com>
2024-02-12 20:30:43 +0100

    feat(models): line_items relation to cart & product

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,5 @@
 class Cart < ApplicationRecord
+  # has_many() method is used to define a one-to-many relationship between the Cart model and the LineItem model. It came from the ActiveRecord module, which is included in the ApplicationRecord class.
+  # The dependent: :destroy option is used to ensure that the removal of a cart will result in the removal of all of its associated line items. 'dependent:' is a parameter that can be passed to the has_many() method. It can take one of the following values: :destroy, :delete, :nullify, :restrict_with_exception, :restrict_with_error. In this case, :destroy is used to ensure that the removal of a cart will result in the removal of all of its associated line items.
+  has_many :line_items, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,15 @@ class Product < ApplicationRecord
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  has_maney :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
