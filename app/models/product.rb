6e69537bcc7a2ef243c23024bac76ab6621b0024
subudei <milan_cirkovic@yahoo.com>
2024-02-18 13:17:01 +0100

    feat(product): add test

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,11 +7,12 @@ class Product < ApplicationRecord
   }
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 
-  has_maney :line_items
+  has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
 
   private
   def ensure_not_referenced_by_any_line_item
+    # unless is a keyword that is used to define a conditional statement. It is used to execute a block of code if a condition is false. In this case, it is used to check if the product has any associated line items. If it does, an error is added to the base attribute of the product and the method returns false, which will prevent the product from being destroyed.
     unless line_items.empty?
       errors.add(:base, 'Line Items present')
       throw :abort
